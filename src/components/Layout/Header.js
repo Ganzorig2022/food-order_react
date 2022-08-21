238dd764d5ad2335e7b8893dd5c05eb26e78ee20
Ganzorig2022 <ganzo.galaxy@gmail.com>
2022-08-21 20:04:14 +0800

    food-order

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './header.module.css';
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='meal image' />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
