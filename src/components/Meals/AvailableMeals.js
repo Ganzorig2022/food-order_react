87790b6ab84b26c402c006c640c36b6d32e441a7
Ganzorig2022 <ganzo.galaxy@gmail.com>
2022-09-04 20:41:12 +0800

    comment

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,38 +1,79 @@
-import classes from './AvailableMeals.module.css';
-import Card from '../UI/Card';
-import MealItem from './MealItem';
+import { useEffect, useState } from 'react';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+import classes from './AvailableMeals.module.css';
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+
+  useEffect(() => {
+    //1. Fetch data
+    const fetchMeals = async () => {
+      const response = await fetch(
+        'https://react-http-ecb71-default-rtdb.firebaseio.com/meals.json'
+      );
+
+      //2. true or false returns
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      //m1: {description: 'Finest fish and veggies', name: 'Sushi', price: 22.99},
+      //m2: {description: "A german specialty! "name: "Schnitzel"price: 16.5 },
+      //m3: {description: 'American, raw, meaty', name: 'Barbecue Burger', price: 12.99 },
+      //m4: {description: 'Healthy...and green...', name: 'Green Bowl', price: 18.99 },
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      //4. Push to new array []
+      for (const key in responseData) {
+        // key =m1,m2,m3,m4
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+
+      //5. Set meal array
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+
+    //6. if something is wrong...
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  //7. loading text
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+  //8. Rendering
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
+      id={meal.id}
       name={meal.name}
       description={meal.description}
       price={meal.price}
---
+++ tree.md
@@ -0,0 +1,17 @@
+// 1. App.js
+
+    1. Header.js
+    2. Cart.js
+    3. Meals.js
+    4. CartProvider -> CartContext
+
+// 1. meals -> MealItem folder
+
+    1. "MealsSummary" ->
+    "MealItem" component (sushi, shushi desription, price ) -> "MealItemForm" component (add to cart button)
+    2. "AvailableMeals" (fetching meal json data)
+    3.
+
+// 2. Layout folder
+
+    1.  "Header" -> "HeaderCartButton" component
